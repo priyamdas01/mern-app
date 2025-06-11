47b99eedf5e2dcb9e67118cd821e7d57b0b78823
priyam01 <12priyamdas34@gmail.com>
2025-06-11 00:42:55 -0400

    mern-app

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import connectDB from "./config/db.js";
+import productRouter from "./routes/product.route.js";
 
 const app = express();
+app.use(express.json());
+app.use("/api/products", productRouter);
+
+console.log(process.env.MONGO_URI);
 
 app.listen(3000, () => {
   connectDB();
   console.log("Server started on http://localhost:3000");
-});
-console.log(process.env.MONGO_URI);
-
-app.get("/", (req, res) => {
-  res.send("server is ready");
 });
